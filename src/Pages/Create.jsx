5e56353220930ae65f0e31504d75defb908c1ca3
Keyur Bhimani <keyurbhimani2000@gmail.com>
2024-05-12 10:37:04 +0530

    First commit

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -11,66 +11,67 @@ const Create = () => {
     const [files, setFiles] = useState();
     const [content, setContent] = useState();
     const [redirect, setRedirect] = useState(false);
-    const [link,setUrl] = useState();
+    const [link, setUrl] = useState();
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
-    function createNewPost(e){
+    function createNewPost(e) {
         e.preventDefault();
         const Data = new FormData();
-        Data.append('file',files);
-        Data.append("upload_preset","blogapp")
-        Data.append("cloud_name","dxqcanezw")
-        fetch('https://api.cloudinary.com/v1_1/dxqcanezw/image/upload',{
+        Data.append('file', files);
+        Data.append("upload_preset", "blogapp")
+        Data.append("cloud_name", "dxqcanezw")
+        fetch('https://api.cloudinary.com/v1_1/dxqcanezw/image/upload', {
             method: 'POST',
             body: Data,
-        }).then(res=>res.json())
-        .then(data=>{
-           setUrl(data.url)
-           console.log(data.url)
-        })
-        .catch(err=>console.log(err))
+        }).then(res => res.json())
+            .then(data => {
+                setUrl(data.url)
+                console.log(data.url)
+            })
+            .catch(err => console.log(err))
         console.log(link);
         link && (
             fetch('https://blogappapi.vercel.app/post', {
                 method: "POST",
-                headers: {"Content-Type" : "application/json"},
-                body: JSON.stringify({title,summary,content,pic:link}),
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ title, summary, content, pic: link }),
                 credentials: 'include'
-            }).then(res=>res.json())
-            .then(data=>{
-                if(data.error){
-                    toast.error(data.error)
-                }else{
-                    setLoading(true)
-                    toast.success("Blog created successfully")
-                    navigate('/')
-                }
-            })
-        ) 
-        
+            }).then(res => res.json())
+                .then(data => {
+                    if (data.error) {
+                        toast.error(data.error)
+                    } else {
+                        setLoading(true)
+                        toast.success("Blog created successfully")
+                        navigate('/')
+                    }
+                })
+        )
+
     }
 
-    if(redirect){
+    if (redirect) {
         return <Navigate to={'/'} />
     }
 
-  return (
-    <div>
-        <Navbar />
+    return (
         <div>
-            <form onSubmit={createNewPost} className="flex flex-col w-[100%] lg:w-[50%] m-auto mt-10 gap-6 p-20" >
-                <input value={title} onChange={(e)=>setTitle(e.target.value)} className="p-2 border-2 border-slate-300 rounded-sm outline-none" type="text" placeholder='Title' />
-                <input value={summary} onChange={(e)=>setSummary(e.target.value)} className="p-2 border-2 border-slate-300 rounded-sm outline-none"type="text" placeholder='Summary' />
-                <input onChange={(e)=>setFiles(e.target.files[0])} className="p-2 border-2 border-slate-300 rounded-sm outline-none" type="file"/>
-                <ReactQuill value={content} onChange={newVal => setContent(newVal)}  />
-                {loading ? 
-                    <button disabled className="w-[100%] bg-slate-600 text-xl font-semibold transition-all duration-300 text-white hover:bg-slate-500 p-2 rounded-md">Loading</button>
-                :    <button className="w-[100%] bg-slate-600 text-xl font-semibold transition-all duration-300 text-white hover:bg-slate-700 p-2 rounded-md">Create Post</button>
-                }
-            </form>
+            <Navbar />
+            <div>
+                <form onSubmit={createNewPost} className="flex flex-col w-[100%] lg:w-[50%] m-auto mt-10 gap-6 p-20" >
+                    <input value={title} onChange={(e) => setTitle(e.target.value)} className="p-2 border-2 border-slate-300 rounded-sm outline-none" type="text" placeholder='Title' />
+                    <input value={summary} onChange={(e) => setSummary(e.target.value)} className="p-2 border-2 border-slate-300 rounded-sm outline-none" type="text" placeholder='Summary' />
+                    <input onChange={(e) => setFiles(e.target.files[0])} className="p-2 border-2 border-slate-300 rounded-sm outline-none" type="file" />
+                    <ReactQuill value={content} onChange={newVal => setContent(newVal)} />
+                    {loading && (
+                        <button disabled className="w-[100%] bg-slate-600 text-xl font-semibold transition-all duration-300 text-white hover:bg-slate-500 p-2 rounded-md">Loading</button>
+                    )}
+                    {!loading && (<button className="w-[100%] bg-slate-600 text-xl font-semibold transition-all duration-300 text-white hover:bg-slate-700 p-2 rounded-md">Create Post</button>
+                    )}
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Create
