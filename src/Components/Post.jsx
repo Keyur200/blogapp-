effbfc7974ea35c4a639e1ba164327c7f8cfbcfc
Keyur Bhimani <keyurbhimani2000@gmail.com>
2024-06-03 12:13:34 +0530

    firs

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -5,10 +5,12 @@ import { useSelector } from 'react-redux';
 const Post = () => {
   const search = useSelector(state=>state.search.search)
   const [posts,setPosts] = useState([]);
+  const [loading,setLoading] = useState(false)
   useEffect(()=>{
     fetch('https://blogappapi.vercel.app/post').then((response)=>{
       response.json().then((posts)=>{
         setPosts(posts)
+        setLoading(true)
       })
     })
   },[])
@@ -17,15 +19,20 @@ const Post = () => {
     <div>
 
       {
-        posts.length > 0 && 
-        
-        posts.filter((i)=>{
-          return ( i.title.toLowerCase().includes(search.toLowerCase()) );
-        }).map((post,i)=>{
-          return (
-            <Blogs key={i} {...post} />
-          )
-        })
+
+        loading ? (
+          posts.length > 0 && 
+          
+          posts.filter((i)=>{
+            return ( i.title.toLowerCase().includes(search.toLowerCase()) );
+          }).map((post,i)=>{
+            return (
+              <Blogs key={i} {...post} />
+            )
+          })
+        ) : (
+          <p>Loading.....</p>
+        )
       }
     </div>
   )
